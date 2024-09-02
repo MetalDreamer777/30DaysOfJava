f3eb2c663d79611aa3183c204932e473ab20c95d
Josiah Wathen <josiah.wathen@gmail.com>
2024-09-02 17:24:31 -0400

    day 5 - collections notes / queue and stack

--- 30DaysOfJava/src/MySweetProgram.java
+++ 30DaysOfJava/src/MySweetProgram.java
@@ -1,56 +1,69 @@
-
+// https://docs.oracle.com/javase/8/docs/api/java/util/Collection.html
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MySweetProgram {
+	
+	
+	List<String> stuff; //we can use this list as anything that implements it
+	ArrayList<String> stuffs;
+	Collection<String> stuf;
 
 	public static void main(String[] args) throws FileNotFoundException {
-		//arrays();
-		//readFromFile();
-		//collectInArrayList();
-		bestPractice();
-		
-	}
-	
-	private static void arrays() {
-		
-		Scanner in = new Scanner(System.in);
-		
-		System.out.println("How many grades?");
-		
-		int size = in.nextInt();
-		int[] grades = new int[size];
-		
-		for (int i = 0; i < size; i++) {
-			System.out.println("Input grade " + (i+1) + ":");
-			grades[i] = in.nextInt();
+		boolean run = true;
+		while (run == true) {
+			Scanner in = new Scanner(System.in);
+			System.out.println(
+					  "Enter a number to choose a method\n"
+					+ "1 : collections\n"
+					+ "2 : linkedListQueue\n"
+					+ "3 : linkedListStack\n");
+			String method = in.nextLine();
+			switch (method) {
+			default:
+				System.out.println("Not a Method");
+				break;
+			case "1":
+				collections();
+				run = false;
+				break;
+			case "2":
+				linkedListQueue();
+				run = false;
+				break;
+			case "3":
+				linkedListStack();
+				run = false;
+				break;
+			}
+			in.close();
 		}
 		
-		System.out.println(Arrays.toString(grades));
-		in.close();
+//		
+//		
+//		
+		
 	}
 	
-	private static void readFromFile() throws FileNotFoundException {
+	public static void collections() throws FileNotFoundException {
 		
 		Scanner in = new Scanner(new File("students.txt"));
 		
-		while(in.hasNextLine()) {
-			System.out.println(in.nextLine());
-		}
+//*
+		Collection<String> studentss = new ArrayList<String>(); //collection is an interface that contains sub-interfaces
+		List<String> studentsss = new ArrayList<String>(); //list is a sub-interface that contains implementations such as ArrayList
+//		Interface<>					  Implementation<>
+//*/
 		
-		in.close();	
-	}
-	
-	public static void collectInArrayList() throws FileNotFoundException {
+		ArrayList<String> students = new ArrayList<String>(); //no reason to be general here
 		
-		Scanner in = new Scanner(new File("students.txt"));
-		
-		List<String> students = new ArrayList<String>();
+		System.out.println("Reading from file...");
 		
 		while(in.hasNextLine()) {
 			students.add(in.nextLine());
@@ -61,34 +74,71 @@ public class MySweetProgram {
 		for (int i = 0; i < students.size(); i++) {
 			System.out.println("Name: " + students.get(i));
 		}
+		in.close();
 		
-		in.close();	
+		MySweetProgram p = new MySweetProgram();
+		p.stuff = new ArrayList<String>(); //this works because ArrayList implements the List interface
+//		p.stuffs = new LinkedList<String>(); //this won't work because LinkedList doesn't implement the ArrayList class, and classes can't be implemented anyways
+//		p.stuf = new List<>(); //this wont work because you can only instantiate classes, not interfaces
+		p.stuf = new ArrayList<String>();
 	}
 	
-	public static void bestPractice() {
+// a stack is first in last out, like doing a stack of dishes
+// a queue is first in first out, like standing in line at a roller coaster	
+	
+	public static void linkedListQueue() {
+		LinkedList<String> names = new LinkedList<String>();
+		//Pulls from opposite end that stuff is added too
+//*/
+		//Queuing
+		names.addLast("Josiah"); //same as .add
+		names.addLast("Caleb");
+		names.addLast("Sue");
+		names.addLast("Sally");
 		
-		try {
-			
-			Scanner in = new Scanner(new File("students.txt"));
-			List<String> students = new ArrayList<String>();
-			
-			while(in.hasNextLine()) {
-				students.add(in.nextLine());
-			}
-			System.out.println(students);
-			
-			for (int i = 0; i < students.size(); i++) {
-				System.out.println("Name: " + students.get(i));
-			}
-			
-			in.close();	
-			
-		} catch (FileNotFoundException e) {
-			
-			System.out.println("File Not Found\n" + e);
-			e.printStackTrace();
-			
+		while (!names.isEmpty()) {
+			System.out.println(names.removeFirst()); //same as .remove
 		}
+//*/
+/*
+		//you could also do .addFirst and .removeLast
+		names.addFirst("Josiah");
+		names.addFirst("Caleb");
+		names.addFirst("Sue");
+		names.addFirst("Sally");
 		
+		while (!names.isEmpty()) {
+			System.out.println(names.removeLast());
+		}
+//*/
+	}
+	
+	public static void linkedListStack() {
+		LinkedList<String> names = new LinkedList<String>();
+		//Pulls from same end that stuff is added too
+//*
+		//Stacking
+		names.addFirst("Josiah");
+		names.addFirst("Caleb");
+		names.addFirst("Sue");
+		names.addFirst("Sally");
+		
+		while (!names.isEmpty()) {
+			System.out.println(names.removeFirst());
+		}
+//*/
+/*
+		//you could do .addLast and .removeLast
+		names.addLast("Josiah");
+		names.addLast("Caleb");
+		names.addLast("Sue");
+		names.addLast("Sally");
+		
+		while (!names.isEmpty()) {
+			System.out.println(names.removeLast());
+		}
+//*/
 	}
 }
+
+
