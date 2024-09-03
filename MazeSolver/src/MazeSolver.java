39ecf3deaf77facd3628e21c0b3da93f0da4038f
Josiah Wathen <josiah.wathen@gmail.com>
2024-09-03 05:19:37 -0400

    day 10 - maze solver path finding and back tracking

--- MazeSolver/src/MazeSolver.java
+++ MazeSolver/src/MazeSolver.java
@@ -1,12 +1,11 @@
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class MazeSolver {
 	
 	static int[][] maze = {
 			{2, 0, 1, 1},
-			{1, 1, 0, 1},
-			{0, 1, 1, 1}
+			{1, 1, 1, 0},
+			{0, 0, 0, 1}
 	};
 	
 	static // 0 = wall
@@ -16,11 +15,60 @@ public class MazeSolver {
 	LinkedList<Position> path = new LinkedList<Position>();
 	
 	public static void main(String[] args) {
-		Position p = new Position(3, 0);
+		Position p = new Position(0, 3);
 		path.push(p);
 		
-		maze[path.peek().y][path.peek().x] = 0;
+		while(true) {
+			int x = path.peek().x;
+			int y = path.peek().y;
+			maze[y][x] = 0;
+			
+			//down
+			if(maze[y+1][x]==2) {
+				System.out.println("Moved Down\nYou Won!");
+				return;
+			}else if(maze[y+1][x]==1) {
+				System.out.println("Moved Down");
+				path.push(new Position(y+1, x));
+				continue;
+			}
+			
+			//left
+			if(maze[y][x-1]==2) {
+				System.out.println("Moved Left\nYou Won!");
+				return;
+			}else if(maze[y][x-1]==1) {
+				System.out.println("Moved Left");
+				path.push(new Position(y, x-1));
+				continue;
+			}
+			
+			//up
+			if(maze[y-1][x]==2) {
+				System.out.println("Moved Up\nYou Won!");
+				return;
+			}else if(maze[y-1][x]==1) {
+				System.out.println("Moved Up");
+				path.push(new Position(y-1, x));
+				continue;
+			}
+			
+			//right
+			if(maze[y][x+1]==2) {
+				System.out.println("Moved Right\nYou Won!");
+				return;
+			}else if(maze[y][x+1]==1) {
+				System.out.println("Moved Right");
+				path.push(new Position(y, x+1));
+				continue;
+			}
+			
+			//backtrack
+			path.pop();
+			if(path.size() == 0) {
+				System.out.println("No Path");
+			}
+		}
 		
-		System.out.println(Arrays.deepToString(maze));
 	}
 }
